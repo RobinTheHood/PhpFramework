e37a460bc99dfec8a0db77b36b59bbc713ee7692
Robin Wieschendorf <mail@robinwieschendorf.de>
2018-01-27 16:34:33 +0100

    Request Class added.

---
+++ src/Request.php
@@ -0,0 +1,40 @@
+<?php
+namespace RobinTheHood\PhpFramework;
+
+class Request
+{
+    public static function get($name, $filter = FILTER_DEFAULT)
+    {
+        return filter_input(INPUT_GET, $name, $filter);
+    }
+
+    public static function getAll()
+    {
+        return filter_input_array(INPUT_GET);
+    }
+
+    public static function getInt($name)
+    {
+        return self::get($name, FILTER_VALIDATE_INT);
+    }
+
+    public static function post($name, $filter = FILTER_DEFAULT)
+    {
+        return filter_input(INPUT_POST, $name, $filter);
+    }
+
+    public static function postAll()
+    {
+        return filter_input_array(INPUT_POST);
+    }
+
+    public static function postInt($name)
+    {
+        return self::post($name, FILTER_VALIDATE_INT);
+    }
+
+    public static function server($name, $filter = FILTER_DEFAULT)
+    {
+        return filter_input(INPUT_SERVER, $name, $filter);
+    }
+}
